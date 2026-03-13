2e111ad2428c5df6343f50ced058ee01c2236692
Raul Diaz <ralk3080@gmail.com>
2026-03-13 11:58:12 -0600

    Se añade proyecto minimalApi

---
+++ UserApp/WeatherForecast.cs
@@ -0,0 +1,12 @@
+namespace UserApp;
+
+public class WeatherForecast
+{
+    public DateOnly Date { get; set; }
+
+    public int TemperatureC { get; set; }
+
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+    public string? Summary { get; set; }
+}
